8a3e940bb91cc75c38f91490d155d65ed49441b3
Riley Mathews <rileymathews80@gmail.com>
2018-04-09 12:09:25 -0500

    added initial inventory items

---
+++ storage.js
@@ -0,0 +1,67 @@
+// Define the database as an object
+const HomeInventoryDatabase = {}
+
+// Define the arrays (a.k.a. tables) in the database to store each type of items
+HomeInventoryDatabase.furniture = []
+HomeInventoryDatabase.crafts = []
+HomeInventoryDatabase.electronics = []
+
+const markVAmp = {
+    name: "Mesa Boogie Mark V Guitar Amp",
+    location: "studio",
+    description: "My first tube amplifier I owned"
+}
+
+const archTopBanjo = {
+    name: "1912 Gibson Arch Top banjo",
+    location: "studio",
+    description: "My oldest instrument, rumored to have previously been owned by banjo virtuoso Noam Pickelny"
+}
+
+const gamingStation = {
+    name: "Gaming Desktop Computer",
+    location: "bedroom",
+    description: "The current iteration of my gaming pc I started building years ago and have upgraded and iterated on ever since"
+}
+
+const acousticGuitar = {
+    name: "Collings D2h dreadnaught guitar",
+    location: "studio",
+    description: "one of my favorite instruments I own, it's back and sides are made of solid Brazillian Rosewood, which is now illegal to import into the US"
+}
+
+const lesPaul = {
+    name: "Les Paul '59 Reissue",
+    location: "studio",
+    description: "my favorite electric guitar, it is a '59 reissue bought from the gibson showcase which used to be in the opry mills mall"
+}
+
+const iMac = {
+    name: "apple iMac desktop computer",
+    location: "studio",
+    description: "my apple iMac which is used exclusively for recording, and other music related projects"
+}
+
+const tenorSax = {
+    name: "selmer markVI tenor saxophone",
+    location: "studio",
+    description: "a vintage selmer saxophone, often hailed as one of the best saxophone models ever produced"
+}
+
+const strat = {
+    name: "Fender american stratocaster",
+    location: "studio",
+    description: "A strat which has undergone many iterations as I have swapped out the pickups, currently using the same pickups as one of my favorite guitarists, Andy Timmons."
+}
+
+const recordingStation = {
+    name: "Recording Desk",
+    location: "studio",
+    description: "A desk I bought along with my iMac to be the foundation for my recording station"
+}
+
+const bookshelf = {
+    name: "bookshelf",
+    location: "bedroom",
+    description: "A gift from my mom when my bookshelf I had throughout college began to fall apart."
+}
